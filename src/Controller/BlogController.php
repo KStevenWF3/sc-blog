ac2ca6ab30358c49541ca9532d235bad292c3a76
K-Steven <82813382+KStevenWF3@users.noreply.github.com>
2021-07-09 09:10:21 +0200

    relation entité + fixtures + dev templates

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,10 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Comment;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\CommentType;
 
 class BlogController extends AbstractController
 {
@@ -31,12 +36,12 @@ class BlogController extends AbstractController
         // $reportArticles = $this->getDoctrine()->getRepository(Article::class); //! remplacer par ArticleRepository $reportArticles en TODO-ONE
 
         //* symfony à sa propre fonction var_dump
-        dump($reportArticles); // dd()
+        // dump($reportArticles); // dd()
 
         //* findall() : SELECT * FROM article + FETCHALL
         //* $articles : tableau ARRAY multidimensionnel contenant l'ensemble des articles stockés dans la BDD
         $articles = $reportArticles->findAll();
-        dump($articles);
+        // dump($articles);
 
         return $this->render('blog/blog.html.twig', [
             // 'controller_name' => 'BlogController',
@@ -61,14 +66,134 @@ class BlogController extends AbstractController
     //!\\ attention si en dessous de /blog/{id} ne fonctionnera pas car le controller attendra un id et prendra new pour un id
     /**
      * Méthode permettant de créer un nouvel article et de modifier un article existant
-     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/new_old", name="blog_create_old")
      */
-    public function create(): Response
+    public function createOld(Request $request, EntityManagerInterface $manager): Response
     {
-        return $this->render('blog/create.html.twig',[
+        dump($request);
+
+        //* la prorpiété "$request->request" permet de stocker et d'accéder aux données saisie dans le formulaire,
+        //* c'est à dire aux données de la surperloable $_POST
+        //* Si les données sont supérieurs à 0; donc si nous avons bien saisie des données dans le formulaire,
+        //* alors on entre dans la condition IF
+        if($request->request->count() > 0)
+        {
+            //* Si nous voulons insérer des données dans la table SQL Article, nous devons instancier et remplir
+            //* un objet issu de son entité correspondante (classe Article)
+            $article = new Article;
+
+            //* On renseigne tout les setteurs de l'objet avec les données saisie dans le formulaire
+            // $request->request->get('titre') //** : permet d'atteindre la valeur du titre saisi
+            //* dans le champ 'titre" du formulaire
+            $article->setTitre($request->request->get('titre'))
+                    ->setContenu($request->request->get('contenu'))
+                    ->setImage($request->request->get('image'))
+                    ->setDate(new \DateTime());
             
+            // dump($article);
+
+            //* Pour manipuler les lignes de la BDD (INSERT, UPDATE, DELETE), nous avons besoin d'un manager (EntityManagerInterface)
+            //* persist() : méthode issue de l'interface EntityManagerInterface permettant de préparer et garder en mémoire la requete d'insertion
+            //* $data = $bdd->prepare("INSERT INTO article VALUES ('$article->geTitre()', '$article->getContenu()')')
+            $manager->persist($article);
+
+            //* flush() : méthode issue de l'interface EntityManagerInterface permettant veritablement d'executer la requete d'insertion en BDD
+            //* $data->execute()
+            $manager->flush();
+
+            dump($article);
+
+            //* Après l'insertion de l'article en BDD, nous redirigeons l'internaute vers l'affichage du détail de l'article,
+            //* donc une autre route via la méthode redirectToRoute()
+            //* Cette méthode attend 2 arguments
+            //? 1. La route
+            //? 2. le paramètre a transmettre dans la route, dans notre cas l'ID de l'article
+            return $this->redirectToRoute('blog_show', [
+                'id' => $article->getId()
+            ]);
+        }
+
+        //* La classe Request permet de stocker et d'avoir accès aux données véhiculées par les superglobales
+        //? ($_POST, $_GET, $_COOKIE, $_FILES, ect...)
+
+        return $this->render('blog/create_old.html.twig');
+    }
+
+
+    /**
+     * Méthode permettant d'afficher le détail d'un article
+     * 
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
+     */
+    public function create(Article $article = null, Request $request, EntityManagerInterface $manager): Response
+    {
+        //* Si la variable $article N'EST PAS (null), si elle ne contient aucun article de la BDD,
+        //* cela veut dire nous avons envoyé la route '/blog/new',
+        //* c'est une insertion, on entre dans le IF et on crée une nouvelle instance de l'entité Article, création d'un nouvel article
+        //* Si la variable $article contient un article de la BDD, cela veut dire que nous avons envoyé la route '/blog/id/edit',
+        //* c'est une modifiction d'article, on entre pas dans le IF, $article ne sera pas null,
+        //* il contient un article de la BDD, l'article à modifier
+        if(!$article)
+        {
+            $article = new Article;
+        }
+        // $article = new Article;
+
+        //TODO TEST [réremplie le formulaire]
+        //* En renseignant les setteurs  de l'entité, on s'aperçoit que les valeurs sont évnoyés directement dans
+        //* ls attributs 'value' du formulaire, cela est dû au fait que l'entité $article est relié au formulaire
+        //  $article->setTitre("Titre bidon")
+        //          ->setContenu("Contenu bidon");
+
+        dump($request); //! ajouté avec (Request $request) après fonctionnement valable, ça récupère les données saisies du form
+        //! permet donc de réintroduire les données dans le form
+
+        //* createForm() permet ici de créer un formulaire d'ajout d'article en fonction de la classe ArticleType
+        //* En 2eme argument de createForm(), nous transmettons l'objet entité $article afin de préciser
+        //* que le formulaire a pour but de remplir l'objet $article, on relie l'entité au formulaire
+        //! importer ArticleType, class crée pour un formulaire sur la table Article
+        $formArticle = $this->createForm(ArticleType::class, $article);
+
+        //* handleRequest() permet ici dans notre cas, de récupérer toute les données saisie dans le formulaire et de les transmetre aux bon seteurs de l'entité $article
+        //* handleRequest() renseigne chaque setteur de l'entité $article avec les données saisi dans le formulaire
+        $formArticle->handleRequest($request); //! et avec les données récup il va les binder directement dans les bon setter de l'objet
+        //! non possible dans la version create_old
+
+        // dump($article);
+
+        //* Si le formulaire a bien été validé && que toute les données saisie sont bien transmise à la bonne entité,
+        //* alors on entre dans la condition IF
+        if($formArticle->isSubmitted() && $formArticle->isValid())
+        {
+            // on renseigne le setteur de la date, puisque nous n'avons pas de champ 'date' dans le formulaire
+            // Si l'article ne possède pas d'ID, alors on entre dans la condition IF et on execute le setteur de la date, on entre dans le IF que dans le cas de la création d'un nouvel article
+            if(!$article->getId())
+            {
+                $article->setDate(new \DateTime());
+            }
+
+            $manager->persist($article);
+
+            $manager->flush();
+
+            //!\\ arrivé là on reste sur la page avec les champs préremplie de ce qu'on vient de renseigner,
+            //! cependant avec la commande qui suit on sera auto redirigé après post
+
+            return $this->redirectToRoute('blog_show', [
+                'id' => $article->getId()
+            ]);
+        }
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $formArticle->createView(),
+            //? on transmet le formulaire au template afin de pouvoir l'afficher avec TWIG
+            //* createView() va retourner un petit objet qui représente l'affichage du formulaire,
+            //* on le récupère dans le template create.html.twig
+            'editMode' => $article->getId() // on transmet l'id de l'article au template
         ]);
     }
+
 
     /**
      * Méthode permettant d'afficher le détail d'un article
@@ -76,7 +201,7 @@ class BlogController extends AbstractController
      * @Route("/blog/{id}", name="blog_show")
      */
     // public function show(ArticleRepository $reportArticle, $id): Response
-    public function show(Article $article): Response
+    public function show(Article $article, Request $request): Response
     {
         //* L'id transmit dans l'URL est envoyé directement en argument de la fonction show(),
         //* ce qui nous permet d'avoir accès à l'id de l'article à selectionner en BDD au sein de la méthode show()
@@ -93,9 +218,22 @@ class BlogController extends AbstractController
         // $article = $reportArticle->find($id); //! reporté directement dans la function non commenté
         // dump($article);
 
+        //* TRAITEMENT COMMENTAIRE ARTICLE (forulaire + insertion)
+        $comment = new Comment;
+
+        $formComment = $this->createForm(CommentType::class, $comment);
+
+        // dump($request);
+
+        $formComment->handleRequest($request);
+
+        // dump($request);
+
+
         return $this->render('blog/show.html.twig', [
-            'articleBDD' => $article
+            'articleBDD' => $article,
             //* on transmet au template les données de l'article selectionné en BDD afin de lestraiter avec le langage Twig dans le template 
+            'formComment' => $formComment->createView()
         ]);
     }
 
---
+++ src/Form/ArticleType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Category;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        //* la fonction add permet de crée les champs du formulaire
+        $builder
+            // ->add('titre', TextareaType::class) //! on change le champ en area (importer la classe)
+            // ->add('contenu', TextType::class, [ //! on change le champ en text (importer la classe)
+            //     'attr' => [
+            //         'placeholder' => "Contenu de l'article"
+            //     ]
+            // ])
+            ->add('titre', TextType::class, [
+                'required' => false,
+                'attr' => [
+                    'placeholder' => "Saisir le titre de l'article",
+                    // 'class' => 'nouvelle.classe.css btn ect', //! modifier l'apaprence malgré le yaml
+
+                ]
+            ])
+            //* On définit le champ qui permet d'associer une catégorie à l'article dans le formulaire
+            //* Ce champ provient d'une autre entité : Category
+            ->add('category', EntityType::class, [
+                'class' => Category::class, //? on précise de quelle entité provient ce champ
+                'choice_label' => 'titre' //? le contenu de la liste déroulante sera le titre des catégories
+            ])
+            ->add('contenu', TextareaType::class, [
+                'required' => false,
+                'label' => "Détail de l'article", // change contenu en ceci
+                'attr' => [
+                    'placeholder' => "Contenu de l'article",
+                    'rows' => 15
+                ]
+            ])
+            ->add('image', TextType::class, [
+                'required' => false,
+                'attr' => [
+                    'placeholder' => "Saisir l'URL de l'image"
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
